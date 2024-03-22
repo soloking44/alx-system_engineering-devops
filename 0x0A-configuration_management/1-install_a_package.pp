ed59073fb2132f5cce2c4665b5c1152c21e6e01e
soloking44 <solop8483@gmail.com>
2024-03-22 06:05:34 -0700

    1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-command     => 'pip3 install Flask==2.1.0',
-path        => '/usr/local/bin:/usr/bin',
-environment => ['PATH=/usr/local/bin:/usr/bin'],
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command     => 'pip3 install Flask==2.1.0',
+  path        => '/usr/local/bin:/usr/bin',
+  environment => ['PATH=/usr/local/bin:/usr/bin'],
+  unless      => 'pip3 show Flask | grep -q "Version: 2.1.0"',
 }

4f07282217a62d4baf01b72c8ec479c62596c978
soloking44 <solop8483@gmail.com>
2024-03-23 01:26:13 -0700

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/pup
+
+# This setup a specific edition of flask (2.1.0)
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}

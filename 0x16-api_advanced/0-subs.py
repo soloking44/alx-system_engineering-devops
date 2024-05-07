b61f940813e58fcb5bf1da0ec86a8929526fa2a9
soloking44 <solop8483@gmail.com>
2024-05-07 08:13:29 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 """This process the query subscribers to a Reddit subreddit."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """It returns the number of subscribers to subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Queries the Reddit API and returns the number of subscribers
+        for a given subreddit
+    If an invalid subreddit is given, the function should return 0
+    """
+    # API endpoint for getting subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    # Set a custom User-Agent to avoid too many requests errors
+    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    # Send a Get request
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False).json()
+    subscribers = response.get('data', {}).get('subscribers')
+
+    if not subscribers:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    return subscribers
